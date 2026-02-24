123598ddb800f7c272273f304a2394b4bb3104dc
LipatovKP <89099547+LipatovKP@users.noreply.github.com>
2026-02-24 21:40:33 +0300

    Delete yandex-reviews-app directory

--- yandex-reviews-app/config/services.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'yandex_business' => [
-        'base_url' => env('YANDEX_BUSINESS_API_BASE_URL', 'https://api.business.yandex.ru'),
-        'token' => env('YANDEX_BUSINESS_OAUTH_TOKEN'),
-        'per_page' => env('YANDEX_BUSINESS_PER_PAGE', 20),
-    ],
-];
